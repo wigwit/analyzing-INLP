463ebea7e9215a84c522e75e89aae372cfbda22d
ssakara <karamali@uw.edu>
2022-05-06 12:08:56 -0700

    adding import of dataset

--- src/load_bert.py
+++ src/load_bert.py
@@ -1,14 +1,18 @@
 import torch
-from transformers import BertModel
+from transformers import BertModel, BertTokenizer,
+
 import logging
 
 #logging.basicConfig(level-logging.INFO) #turn on detailed logging
-tokenizer = BertTokenizer.from_pretrained('bert_based_uncased')
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 model = BertModel.from_pretrained('bert-base-uncased',
                                           output_hidden_states = True, # Whether the model returns all hidden-states.
                                                                             )
 
 #load dataset
+from datasets import load_dataset
+dataset = load_dataset("conll2012_ontonotesv5")
+
 inputs = tokenizer(dataset, return_tensors="pt")
 outputs = model(**inputs)
 last_hidden_states = outputs.last_hidden_state
