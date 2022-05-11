880dd769cd51c663c89cce65058d5bca97a3a34e
wigwit <wigwit@g.ucla.edu>
2022-05-11 00:34:37 -0700

    adding data as saved csv files and bert encoder

--- src/load_bert.py
+++ src/load_bert.py
@@ -1,35 +1,60 @@
 import torch
-from transformers import BertModel, BertTokenizer
-
+from transformers import AutoTokenizer, AutoModel
+import sys
 import logging
+from datasets import load_dataset
+import pandas as pd
+import itertools
+import pdb
 
 #logging.basicConfig(level-logging.INFO) #turn on detailed logging
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-model = BertModel.from_pretrained('bert-base-uncased',
-                                          output_hidden_states = True, # Whether the model returns all hidden-states.
-                                                                            )
 
-#load dataset
-from datasets import load_dataset
-train_data = load_dataset("conll2012_ontonotesv5",'english_v12')['train']
-dev_data = load_dataset("conll2012_ontonotesv5",'english_v12')['validation']
-test_data = load_dataset("conll2012_ontonotesv5",'english_v12')['test']
 
-#encode data into tokenizable format
+## defining tokenizer and bert model here
+tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased", use_fast=False)
+model = AutoModel.from_pretrained('bert-base-uncased')
+# for now i disable the hidden states function since we are only using bert to probe                                    
+# output_hidden_states = True, # Whether the model returns all hidden-states)
 
-def encode(doc):
-    return {'sentences': doc['sentences']} #fix this to form a dict of lists of strings
+
+
+def bert_tokenization(words):
+    '''
+    this is a tokenization function that takes in a df.series object 
+    and return a bert input format
+    '''
+    preprocessed_list = words.tolist()
+    # this line of code is little buggy, will come back later
+    tokens = tokenizer.batch_encode_plus(preprocessed_list,padding=True)
+    input_seq = torch.tensor(tokens['input_ids'])
+    input_mask = torch.tensor(tokens['attention_mask'])
+    return input_seq, input_mask
+
+
+# loading data from saved csv files
+train_df = pd.read_csv('../data/train.csv')
+train_input = train_df['words']
+train_pos = train_df['pos_tags']
+train_seq,train_mask = bert_tokenization(train_input)
+
+
+
+# def words_of(dataset):
+#     return {'sentences': doc['sentences']} #fix this to form a dict of lists of strings
 #also check how you did this last year
-#and maybe meet with qingxia if you need
+# and maybe meet with qingxia if you need
+# sentences = [sentence['words'] for doc in train_data for sentence in doc['sentences']]
 
-sentences = [sentence['words'] for doc in train_data for sentence in doc['sentences']]
-inputs = tokenizer(sentences, return_tensors="pt")
+
+# inputs = tokenizer(sentences, return_tensors="pt")
 
 #outputs = model(**inputs) #not sure when we need this
 
 #train the classifier on the tasks to return protected attributes
 
 #use last_hidden_states and protected attributes as input for INLP loop
+
+
 
 
 
---
+++ src/saving_data.py
@@ -0,0 +1,28 @@
+from datasets import load_dataset
+import pandas as pd
+import itertools
+
+def from_ds_to_df(ds,filename=None):
+    '''
+    This functions takes in the dataset object with only 'sentences' and returns
+    a pd.DataFrame object that removes empty srl_frames
+    '''
+    ## ds is a list of list, total set concatenate all the list together
+    ## might want to change later ? since bert is training on A B sents
+    total_set = list(itertools.chain(*ds))
+    df = pd.DataFrame(total_set)
+    filtered_df = df[df['srl_frames'].str.len()>0]
+    if filename is not None:
+        filtered_df.to_csv(filename)
+    return filtered_df
+
+
+#load datasets
+#check if the split is consistent
+train_data = load_dataset("conll2012_ontonotesv5",'english_v4')['train']['sentences']
+dev_data = load_dataset("conll2012_ontonotesv5",'english_v4')['validation']['sentences']
+test_data = load_dataset("conll2012_ontonotesv5",'english_v4')['test']['sentences']
+
+train_df = from_ds_to_df(train_data,'../data/train.csv')
+dev_df =from_ds_to_df(dev_data,'../data/dev.csv')
+test_df = from_ds_to_df(test_data,'../data/test.csv')
