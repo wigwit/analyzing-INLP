ef9f0dde71f707df2d1f54e69a7c6bff70b9c9ec
ssakara <karamali@uw.edu>
2022-05-12 09:21:39 -0700

    bug fixes

--- src/LinearClassifier.py
+++ src/LinearClassifier.py
@@ -1,7 +1,8 @@
+import torch
 class LinearClassifier(torch.nn.Module):
-    def __init__(self, input_dimension):
-                super().__init__()
-                        self.linear = torch.nn.Linear(input_dimension, 1)
-                        def forward(self, input_dimension):
-                                    return self.linear(input_dimension)
+	def __init__(self, input_dimension):
+		super().__init__()
+		self.linear = torch.nn.Linear(input_dimension, 1)
+	def forward(self, input_dimension):
+		return self.linear(input_dimension)
 
