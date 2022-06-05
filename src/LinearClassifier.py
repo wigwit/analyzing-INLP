ddfcf56f03ef99be6184b283ea9f98b3e9546e10
Qingxia Guo <qg07@dryas.ling.washington.edu>
2022-06-04 18:22:54 -0700

    fix bugs in dim size and adding classifier objects

--- src/LinearClassifier.py
+++ src/LinearClassifier.py
@@ -1,8 +1,47 @@
 import torch
+from sklearn.metrics import accuracy_score
+
 class LinearClassifier(torch.nn.Module):
-	def __init__(self, input_dimension):
+	def __init__(self, input_embeddings,output,tag_size):
+		'''
+		a linear classifier for probe
+		input_embeddings : a tensor with size [batch_size,embed_dim]
+		output : a tensor with size [batch_size,1]
+		tag_size : number of classes
+		'''
 		super().__init__()
-		self.linear = torch.nn.Linear(input_dimension, 1)
-	def forward(self, input_dimension):
-		return self.linear(input_dimension)
+		self.embeddings = input_embeddings
+		self.output = output
+		self.linear = torch.nn.Linear(input_embeddings.shape[1], tag_size)
+		# TODO: possible class weight in loss function
+		self.loss_func = torch.nn.CrossEntropyLoss()
+	def forward(self,embeddings):
+		# embedding size = [batch_size, embed_dim]
+		# output size = [batch_size, num_tags]
+		fc = self.linear(embeddings)
+		#TODO: maybe later for logistics regression, output should be one hot
+		#output_probs = torch.nn.functional.softmax(fc,dim=1)
+		return fc
+	
+	def optimize(self,lr=0.01,num_epochs=100):
+		optimizer = torch.optim.SGD(self.linear.parameters(), lr = lr)
+		best_predictions = None
+		for epoch in range(num_epochs):
+			# TODO: shuffle the index of embeddings
+			preds = self.forward(self.embeddings)
+			loss = self.loss_func(preds,self.output)
+			optimizer.zero_grad()
+			loss.backward(retain_graph=True)
+			optimizer.step()
+			print(f'epoch: {epoch+1}, loss = {loss.item():.4f}')
+			# TODO: implement stopping criterion
+			best_predictions= preds
+		final_pred = torch.argmax(best_predictions,dim=1).numpy()
+		final_out = self.output.numpy()
+		print(f'accuracy score:{accuracy_score(final_out,final_pred):.4f}')
+		
+
+
+# class INLPTraining(LinearClassifier):
+# 	def __init__(self,):
 
--- src/probe.py
+++ src/probe.py
@@ -12,6 +12,8 @@ from collections import defaultdict
 from utils import bert_tokenization, get_projection_to_intersection_of_nullspaces, get_rowspace_projection, train_linear_classifier
 import numpy as np
 from tqdm import tqdm
+from sklearn import preprocessing
+from LinearClassifier import LinearClassifier
 #logging.basicConfig(level-logging.INFO) #turn on detailed logging
 
 ## defining GPU here
@@ -21,53 +23,86 @@ device = torch.device("cuda") if torch.cuda.is_available() else torch.device('cp
 #load data and BERT embeddings as in load_bert.py
 ## defining tokenizer and bert model here
 tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased", use_fast=True)
-model = AutoModel.from_pretrained('bert-base-uncased')
-model = model.to(device)
+# model = AutoModel.from_pretrained('bert-base-uncased')
+# model = model.to(device)
 
 # loading data from saved pickle files
-train_df = pd.read_pickle('../data/train.pkl')
-train_input = train_df['words'].tolist()
+train_df = pd.read_pickle('../data/pmb_gold/gold_train.pkl')
+#train_df =train_df[:1000]
+train_input = train_df['text'].tolist()
 
-seq_lens = [len(item) for item in train_input]
-max_len = 75
 
-train_pos = train_df['pos_tags'].tolist()
+train_ccg = train_df['ccg_tags'].tolist()
 
-## For Lindsay: consider this as the possible input for the function
-train_srl = train_df['srl_frames'].tolist()
+train_words=train_df['text'].tolist()
+count_words= [len(l) for l in train_words]
 
-## input for the model
+ccg_encoder = preprocessing.LabelEncoder()
+ccg_encoder.fit(np.concatenate(train_ccg))
+
+ccg_output =[ ccg_encoder.transform(l) for l in train_ccg]
+
+train_st = train_df['semantics_tags'].tolist()
+
+# ## input for the model
 tokens, train_seq,train_mask = bert_tokenization(train_input, tokenizer)
+# #tokens = tokens.to(device)
+# train_seq = train_seq.to(device)
+# train_mask = train_mask.to(device)
 
-outputs = model(train_seq,attention_mask = train_mask)
-word_inds = [tokens.words(i) for i in range(tokens['input_ids'].shape[0])]
+# outputs = model(train_seq,attention_mask = train_mask)
+output = torch.load('../data/pmb_gold/gold_train_embeddings.pt')
+# output = output[:1000]
+word_inds = [tokens.words(i) for i in range(train_seq.shape[0])]
 
 #convert to list of embeddings
 d_list = []
+embed_by_sent = []
+skip_ind = []
 for i in range(len(word_inds)):
 	d = defaultdict(list)
-	for ind,emb in zip(word_inds[i], outputs['last_hidden_state'][i]):
+	for ind,emb in zip(word_inds[i], output[i]):
 		if isinstance(ind, int):
 			d[ind].append(emb)
+	
+	word_in_sent = [torch.mean(torch.stack(d[k],0),0) if len(d[k])>1 else d[k][0] for k in d.keys()]
+	if len(word_in_sent)!= count_words[i]:
+		skip_ind.append(i)
+		continue
+	embed_by_sent.append(torch.stack(word_in_sent,0))
 	d_list.append(d) 
-embeddings = torch.stack([torch.mean(torch.stack(d_list[i][j],0),0) if len(d_list[i][j]) > 1 else d_list[i][j][0] for i in range(len(d_list)) for j in d_list[i].keys()], 0)
-y = torch.tensor([item for sublist in train_pos for item in sublist], dtype=torch.long)
-#(not doing srl yet)
+# 30k*768
+embeddings = torch.cat(embed_by_sent)
+# 30k*1
+y = torch.tensor([item for i, sublist in enumerate(ccg_output) if i not in skip_ind for item in sublist ], dtype=torch.long)
 
+#(not doing srl yet)
+#print(count_words)
+# print(embeddings.shape)
+# print(y.shape)
+# sys.exit()
 #set variables for INLP loop
-num_epochs = 3
-num_tags = 49
+num_epochs = 100
+num_tags = len(ccg_encoder.classes_)
 input_dim = embeddings.shape[1] #should be 768
 orig_embeddings = embeddings
+
+lin_class = LinearClassifier(embeddings,y,num_tags)
+lin_class.optimize()
+lin_class.optimize()
+lin_class.optimize()
+sys.exit()
+
 Ws = []
 rowspace_projections = []
-INLP_iterations = 10 #arbitrary choice for now
+INLP_iterations = 3 #arbitrary choice for now
 min_accuracy = 0.0 #can tune this as well
 
 #INLP loop
 for i in tqdm(range(INLP_iterations)):
 	#y are the gold standard labels for pos or srl
 	linear_model, accuracy = train_linear_classifier(embeddings, y, num_epochs, num_tags, input_dim) #train linear classifier
+	print('accuracy:'+str(accuracy))
 	if accuracy < min_accuracy:
 		continue 
 	W = linear_model.weight
