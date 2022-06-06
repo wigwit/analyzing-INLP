a829ad80537f1453ec3a515d5ade570bca1b288f
Qingxia Guo <qg07@dryas.ling.washington.edu>
2022-06-06 13:34:19 -0700

    adding eval classifier

--- src/LinearClassifier.py
+++ src/LinearClassifier.py
@@ -7,7 +7,7 @@ from typing import List
 import tqdm
 
 class LinearClassifier(torch.nn.Module):
-	def __init__(self, input_embeddings,output,tag_size,dev_x=None,dev_y=None):
+	def __init__(self, input_embeddings,output,tag_size):
 		'''
 		a linear classifier for probe
 		input_embeddings : a tensor with size [batch_size,embed_dim]
@@ -24,22 +24,12 @@ class LinearClassifier(torch.nn.Module):
 		# cls_weight = compute_class_weight('balanced',classes=np.array(range(tag_size)),y=output.numpy())
 		# cls_weight = torch.tensor(cls_weight,dtype=torch.float)
 		self.loss_func = torch.nn.CrossEntropyLoss()
-		self.dev_x = dev_x
-		self.dev_y = dev_y
 	
 	def forward(self,embeddings):
 		# embedding size = [batch_size, embed_dim]
-		# output size = [batch_size]
+		# output size = [batch_size,tag_size]
 		fc = self.linear(embeddings)
-		#TODO: maybe later for logistics regression, output should be one hot
-		#output_probs = torch.nn.functional.softmax(fc,dim=1)
 		return fc
-
-	def eval(self):
-		with torch.no_grad():
-			dev_pred = self.linear(self.dev_x)
-			loss = self.loss_func(dev_pred,self.dev_y)
-		return dev_pred,loss.item()
 
 	
 	def optimize(self,lr=0.01,num_epochs=100):
@@ -53,15 +43,12 @@ class LinearClassifier(torch.nn.Module):
 			optimizer.zero_grad()
 			loss.backward(retain_graph=True)
 			optimizer.step()
-			#print(f'epoch: {epoch+1}, loss = {loss.item():.4f}')
+			print(f'epoch: {epoch+1}, loss = {loss.item():.4f}')
 			#implement stopping criterion
-			dev_pred,dev_loss=self.eval()
-			#print(f'epoch: {epoch+1}, dev loss = {dev_loss:.4f}')
 			if loss.item()<best_loss:
 				best_loss=loss.item()
 				best_model= self.linear
 				best_predictions = preds
-				#best_dev = dev_pred
 				stop_count=0
 			else:
 				if stop_count ==3:
@@ -69,13 +56,11 @@ class LinearClassifier(torch.nn.Module):
 				else:
 					stop_count+=1
 		final_pred = torch.argmax(best_predictions,dim=1).numpy()
-		dev_pred = best_model(self.dev_x)
 		#final_dev = torch.argmax(best_dev,dim=1).numpy()
-		final_dev = torch.argmax(dev_pred,dim=1).numpy()
 		#final_out = output.numpy()
 		#dev_out = self.dev_y.numpy()
 		train_acc = accuracy_score(self.output.numpy(),final_pred)
-		#print(f'train accuracy score:{train_acc:.4f}')
+		print(f'train accuracy score:{train_acc:.4f}')
 		#print(f'dev accuracy score:{accuracy_score(self.dev_y.numpy(),final_dev):.4f}')
 			
 		return best_model,train_acc
@@ -83,9 +68,11 @@ class LinearClassifier(torch.nn.Module):
 
 
 class INLPTraining(LinearClassifier):
-	def __init__(self,input_embeddings,output,tag_size,dev_x=None,dev_y=None):
-		super().__init__(input_embeddings,output,tag_size,dev_x,dev_y)
+	def __init__(self,input_embeddings,output,tag_size):
+		super().__init__(input_embeddings,output,tag_size)
 		self.input_dim = self.embeddings.shape[1]
+		# only used for the applying projection
+		self.original_embedding = input_embeddings.T
 	
 	def get_rowspace_projection(self,model_weight):
 		W = model_weight
@@ -121,9 +108,8 @@ class INLPTraining(LinearClassifier):
 		'''
 		applying projection of P to the embedding vectors
 		'''
-		old_embeddings = self.embeddings.T
 		P = torch.tensor(P,dtype=torch.float)
-		self.embeddings =  torch.matmul(P,old_embeddings).T
+		self.embeddings =  torch.matmul(P,self.original_embedding).T
 
 	def run_INLP_loop(self,iteration,min_acc=0.0):
 		I = np.eye(self.input_dim)
@@ -142,7 +128,6 @@ class INLPTraining(LinearClassifier):
 			rowspace_projections.append(P_rowspace_wi)
 			# Maybe should be optional, but it is described in the original
 			P = self.get_projection_to_intersection_of_nullspaces(rowspace_projections)
-			# this is a little inconsistent with paper and code
 			self.apply_projection(P)
 		
 		return P, rowspace_projections, Ws
---
+++ src/eval_classifier.py
@@ -0,0 +1,83 @@
+from LinearClassifier import LinearClassifier
+import torch
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler
+from sklearn.metrics import accuracy_score
+
+class EvalClassifier(LinearClassifier):
+    def __init__(self,input_embeddings,output,tag_size,dev_x,dev_y):
+        '''
+        A Eval Classifier for eval task, inherited from Linear Classifer
+        But With batching and different logistics regression
+        '''
+        super().__init__(input_embeddings,output,tag_size)
+        self.dev_x = dev_x
+        self.dev_y = dev_y
+        # TODO: tranfer output to one hot vector
+        #self.output_vecs = 
+        #self.loss_func = torch.nn.NLLLoss()
+    
+    def forward(self,embeddings):
+        fc = self.linear(embeddings)
+        #output_probs = torch.nn.functional.softmax(fc,dim=1)
+        return fc
+    
+    def batching_data(self,dset='train',batch_size=32):
+        if dset == 'train':
+            data = TensorDataset(self.embeddings,self.output)
+        elif dset == 'dev':
+            data = TensorDataset(self.dev_x,self.dev_y)
+        sampler = RandomSampler(data)
+        dataloader = DataLoader(data,sampler=sampler,batch_size=batch_size)
+        return dataloader
+    
+    def eval(self):
+        with torch.no_grad():
+            dev_pred = self.forward(self.dev_x)
+            loss = self.loss_func(dev_pred,self.dev_y)
+        return dev_pred, loss.item()
+    
+    def optimize(self,batch_size=32, lr=0.01,num_epochs=100):
+        optimizer = torch.optim.LBFGS(self.linear.parameters(), lr = lr)
+        best_predictions_tr = None
+        best_predictions_dv = None
+        best_loss=float('inf')
+        stop_count = 0
+        for epoch in range(num_epochs):
+            train_batch = self.batching_data(batch_size=batch_size)
+            train_loss = 0
+            train_preds = []
+            dev_preds = []
+            for emb, label in train_batch:
+                pred = self.forward(emb)
+                train_preds.append(pred)
+                loss = self.loss_func(pred,label)
+                self.linear.zero_grad()
+                loss.backward(retain_graph=True)
+                # clip the gradient
+                torch.nn.utils.clip_grad_norm_(self.linear.parameters(), 1.0)
+                optimizer.step()
+                train_loss+=loss.item()
+            avg_loss = train_loss/len(train_batch)
+            print(f'epoch: {epoch+1}, train loss = {avg_loss:.4f}')
+
+            dev_preds,dev_loss = self.eval()
+            print(f'epoch: {epoch+1}, dev loss = {dev_loss:.4f}')
+            if dev_loss<best_loss:
+                # train_preds = torch.cat(train_preds)
+                # dev_preds = torch.cat(dev_preds)
+                best_predictions_tr = torch.cat(train_preds)
+                best_predictions_dv = dev_preds
+                best_model=self.linear
+                stop_count=0
+            else:
+                if stop_count == 3:
+                    break
+                else:
+                    stop_count+=1
+            
+        final_train = torch.argmax(best_predictions_tr,dim=1).numpy()
+        final_dev = torch.argmax(best_predictions_dv,dim=1).numpy()
+        print(f'train accuracy score:{accuracy_score(self.output.numpy(),final_train):.4f}')
+        print(f'dev accuracy score:{accuracy_score(self.dev_y.numpy(),final_dev):.4f}')
+
+        
